5d39ededbdc33ce8f0f2bedb99296c56834d7d4f
Adrian Hill <adrian.hill@mailbox.org>
2021-12-08 19:39:40 +0100

    Add solution of day 7

---
+++ src/day07.jl
@@ -0,0 +1,14 @@
+using Statistics: median
+
+# Data loading
+load(::Day{7}, path) = parse.(Int, split(readline(path), ","))
+
+# Part 1 - fuel of crab submarines
+solve1(::Day{7}, pos) = sum(abs.(pos .- Int(median(pos))))
+
+# Part 2 - fuel of crab submarines 2
+@inline triag(n) = div(n * (n + 1), 2) # fuel spent is a triangular number
+solve2(::Day{7}, pos) = minimum(sum(triag.(abs.(pos .- h))) for h in 0:maximum(pos))
+
+testresult1(::Day{7}) = 37
+testresult2(::Day{7}) = 168
