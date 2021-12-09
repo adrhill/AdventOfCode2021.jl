6f1242fc93d04dc58d4b162f50f1a3997b7f28af
Adrian Hill <adrian.hill@mailbox.org>
2021-12-09 22:46:31 +0100

    Remove unused code

--- src/dispatch.jl
+++ src/dispatch.jl
@@ -7,12 +7,6 @@ data(D::Day{N}) where {N} = load(D, @sprintf("../data/data%02d.txt", N))
 
 test1(D::Day) = solve1(D, testdata(D))
 test2(D::Day) = solve2(D, testdata(D))
-test1(d::Int) = test1(Day(d))
-test2(d::Int) = test2(Day(d))
-testresult1(d::Int) = testresult1(Day(d))
-testresult2(d::Int) = testresult2(Day(d))
 
 solve1(D::Day) = solve1(D, data(D))
 solve2(D::Day) = solve2(D, data(D))
-solve1(d::Int) = solve1(Day(d))
-solve2(d::Int) = solve2(Day(d))
