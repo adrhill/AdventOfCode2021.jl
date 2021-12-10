9e6620ec67e17fb2b1e45dfd6b3cd56d87a77ed7
Adrian Hill <adrian.hill@mailbox.org>
2021-12-10 15:01:02 +0100

    Fix in-place updates of day 10

--- src/day10.jl
+++ src/day10.jl
@@ -5,20 +5,18 @@ load(::Day{10}, path) = [first.(l) for l in split.(readlines(path), "")]
 
 # Part 1 – find corrupted codes: lines where a chunk closes with the wrong character
 char_score = Dict(')' => 3, ']' => 57, '}' => 1197, '>' => 25137)
-match_opening = Dict(')' => '(', ']' => '[', '}' => '{', '>' => '<')
-match_closing = Dict(v => k for (k, v) in match_opening)
-
+match_closing = Dict('(' => ')', '[' => ']', '{' => '}', '<' => '>')
 is_opening(c::Char) = c in "([{<"
 
-function error_score(line)
-    open_chunks = [popfirst!(line)]
+function error_score!(line)
+    expected_closing = Char[]
     while !isempty(line)
         c = popfirst!(line)
         if is_opening(c)
-            push!(open_chunks, c)
+            pushfirst!(expected_closing, match_closing[c])
         else
-            if match_opening[c] == open_chunks[end] # found char matches expected
-                pop!(open_chunks)
+            if c == first(expected_closing)
+                popfirst!(expected_closing)
             else
                 return char_score[c]
             end
@@ -27,27 +25,30 @@ function error_score(line)
     return 0 # zero error score if no error was found
 end
 
-solve1(::Day{10}, data) = sum(error_score.(data))
+function solve1(::Day{10}, data)
+    data = deepcopy(data) # in-place `solve1!` could comment out this line for performance
+    return sum(error_score!.(data))
+end
 
 # Part 2 – autocomplete correct chunks
 char_score2 = Dict(')' => 1, ']' => 2, '}' => 3, '>' => 4) # new scoring rules
 
-# we only need to modify `error_score` from part 1
-function autocomplete(line)
-    open_chunks = [popfirst!(line)]
+# we only need to modify the outputs of `error_score` from part 1
+function autocomplete!(line)
+    expected_closing = Char[]
     while !isempty(line)
         c = popfirst!(line)
         if is_opening(c)
-            push!(open_chunks, c)
+            pushfirst!(expected_closing, match_closing[c])
         else
-            if match_opening[c] == open_chunks[end]
-                pop!(open_chunks)
+            if c == first(expected_closing)
+                popfirst!(expected_closing)
             else
                 return nothing # error was found
             end
         end
     end
-    return reverse(map(c -> match_closing[c], open_chunks))
+    return expected_closing
 end
 
 function score_completion(completion)
@@ -60,8 +61,9 @@ function score_completion(completion)
 end
 
 function solve2(::Day{10}, data)
-    scores = score_completion.(filter(!isnothing, autocomplete.(data))) # filter errors
-    return partialsort!(scores, div(length(scores), 2, RoundUp))
+    data = deepcopy(data) # in-place `solve2!` could comment out this line for performance
+    scores = score_completion.(filter(!isnothing, autocomplete!.(data))) # filter errors
+    return partialsort!(scores, div(length(scores), 2, RoundUp)) # same as `median!`
 end
 
 testresult1(::Day{10}) = 26397
