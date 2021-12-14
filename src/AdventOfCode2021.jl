aa04a9833bc65a6aae5ac9b43bc457309fe453c8
Adrian Hill <adrian.hill@mailbox.org>
2021-12-14 11:24:07 +0100

    Update solution of day 14 for part 2

--- src/AdventOfCode2021.jl
+++ src/AdventOfCode2021.jl
@@ -1,7 +1,7 @@
 module AdventOfCode2021
 using Printf
 
-const CURRENT_DAY = 13
+const CURRENT_DAY = 14
 
 include("dispatch.jl")
 for day in 1:CURRENT_DAY
--- src/day14.jl
+++ src/day14.jl
@@ -1,7 +1,22 @@
 # Day 14: Extended Polymerization
+function addinsert!(d::Dict, key, val)
+    if haskey(d, key)
+        d[key] += val
+    else
+        d[key] = val
+    end
+end
+
 function load(::Day{14}, path)
     lines = readlines(path)
     polymer = first.(split(lines[1], ""))
+
+    pairs = Dict{Vector{Char},Int}()
+    for i in 1:(length(polymer) - 1)
+        pair = polymer[i:(i + 1)]
+        addinsert!(pairs, pair, 1)
+    end
+
     rules = Dict(
         map(lines[3:end]) do l
             ins, out = split(l, " -> ")
@@ -9,41 +24,45 @@ function load(::Day{14}, path)
         end,
     )
 
-    return polymer, rules
+    return pairs, rules, polymer[end]
 end
 
-# Part 1 – score polymer after 10 steps
-function pair_insert(polymer, rules)
-    n = length(polymer)
-    out = Vector{Char}(undef, 2 * n - 1)
-
-    for i in 0:(n - 2)
-        out[2 * i + 1] = polymer[i + 1]
-        out[2 * i + 2] = rules[polymer[(i + 1):(i + 2)]]
+# Part 1 & 2 are the same – at least after rewriting part 1
+function pair_insert(pairs, rules)
+    new_pairs = Dict{Vector{Char},Int}()
+    for (pair, count) in pairs
+        elem = rules[pair]
+        addinsert!(new_pairs, [pair[1], elem], count)
+        addinsert!(new_pairs, [elem, pair[2]], count)
     end
-    out[end] = polymer[end]
-    return out
+    return new_pairs
 end
 
-function score_polymer(polymer::Vector{Char})
-    elements = unique(polymer)
-    counts = [count(==(elem), polymer) for elem in elements]
-    min, max = extrema(counts)
+function score_polymer(pairs, last_elem)
+    element_count = Dict{Char,Int}()
+    for (pair, count) in pairs
+        addinsert!(element_count, pair[1], count)
+    end
+    addinsert!(element_count, last_elem, 1)
+    min, max = extrema(values(element_count))
     return max - min
 end
 
-function polymerize(data, steps)
-    polymer, rules = data
+function polymerize(pairs, rules, steps)
     for _ in 1:steps
-        polymer = pair_insert(polymer, rules)
+        pairs = pair_insert(pairs, rules)
     end
-    return polymer
+    return pairs
 end
 
-solve1(::Day{14}, data) = score_polymer(polymerize(data, 10))
-
-# Part 2
-# solve1(::Day{14}, data) = score_polymer(polymerize(data, 40))
+function solve1(::Day{14}, data)
+    pairs, rules, last_element = data
+    return score_polymer(polymerize(pairs, rules, 10), last_element)
+end
+function solve2(::Day{14}, data)
+    pairs, rules, last_element = data
+    return score_polymer(polymerize(pairs, rules, 40), last_element)
+end
 
 testresult1(::Day{14}) = 1588
 testresult2(::Day{14}) = 2188189693529
